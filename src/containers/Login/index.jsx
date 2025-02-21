f96e55ec94a98abce9a70e7cafd5a769d78c2157
EikeBlox <ezequielgamebr124@gmail.com>
2025-02-21 11:45:08 -0300

    Adição das divs, título e links em style.js

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,10 +1,34 @@
-import { Container } from './styles'
+import { Button, Container, Form, InputContainer, LeftContainer, Link, RightContainer, Title } from './styles'
+import Logo from '../../'
 
 export function Login(){
     return (
         <div>
             <Container>
-                <h1>Login</h1>
+               <LeftContainer>
+                    <h1>Login</h1>
+                    <img scr={Logo} alt="logo-devburger"></img>
+               </LeftContainer>
+               <RightContainer>
+                    <Title>
+                        Olá, seja bem-vindo ao <span>Dev Burguer!</span> Acesse com seu
+                        <span>Login e senha</span>.
+                    </Title>
+                    <Form>
+                        <InputContainer>
+                            <label>Email</label>
+                            <input type="email"></input>
+                        </InputContainer>
+
+                        <InputContainer>
+                            <label>Senha</label>
+                            <input type="password"></input>
+                        </InputContainer>
+                        <Link>Esqueci minha senha.</Link>
+                        <Button>Entrar</Button>
+                        <Link>Não possui conta? Clique aqui.</Link>
+                    </Form>
+               </RightContainer>
             </Container>
         </div>
     )
--- src/containers/Login/styles.js
+++ src/containers/Login/styles.js
@@ -1,5 +1,22 @@
 import styled from 'styled-components';
+import BackgroundLogin from '../../../assets/Background-login.svg'
 
-export const Container = styled.div`
-    background: transparent;
+export const Container = styled.div `
+   display: flex;
+   height: 100vh;
+   width: 100vw;
 `;
+
+export const LeftContainer = styled.div ``;
+
+export const RightContainer = styled.div ``;
+
+export const Title = styled.h2 ``;
+
+export const Form = styled.div ``;
+
+export const InputContainer = styled.h2 ``;
+
+export const Link = styled.a ``;
+
+export const Button = styled.button ``;
